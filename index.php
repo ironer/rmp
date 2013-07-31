e79a3acbd9f2f22af0556136bc1204c2a4fdf572
ironer <ironer80@gmail.com>
2013-07-31 15:01:21 +0200

    Future class. WIP 1

--- index.php
+++ index.php
@@ -35,6 +35,7 @@ if (DEBUG) {
 			. ' / <a href="' . WEBROOT . '">homepage</a> / <a href="' . WEBROOT . '?mail=1">odeslat email</a>'
 			. ' / <a href="' . WEBROOT . '?imap=1">zpracovat imap</a>'
 			. ' / <a href="' . WEBROOT . '?xls=1">xls</a>'
+			. ' / <a href="' . WEBROOT . '?future=1">future</a>'
 			. ' / <a href="' . WEBROOT	. '?read=1">nacist odeslani</a>' . "\n";
 
 	if (!ADVANCEDLOG) App::dump($app);
@@ -47,5 +48,6 @@ if (DEBUG) {
 			. ' / <a href="' . WEBROOT . '">homepage</a> / <a href="' . WEBROOT . '?mail=1">odeslat email</a>'
 			. ' / <a href="' . WEBROOT . '?imap=1">zpracovat imap</a>'
 			. ' / <a href="' . WEBROOT . '?xls=1">xls</a>'
+			. ' / <a href="' . WEBROOT . '?future=1">future</a>'
 			. ' / <a href="' . WEBROOT	. '?read=1">nacist odeslani</a>' . "\n";
 }
